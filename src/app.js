27a6b329dfa1692ec94ae5f00b7b6f794c39e2fd
AnandGoyal264 <anandgoyal9171@gmail.com>
2026-01-18 13:36:35 +0530

    routes_controllers_schema_everything_is_working_fine_check_with_postman_

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import express from "express";
 import cors from "cors";
-import connecttoDB from './config/db.js'
-import dotenv from "dotenv";
-dotenv.config();
 
-const app=express()
-connecttoDB()
-app.listen(process.env.port,()=>{
-    console.log("port has been initialised")
+import authRoutes from "./routes/auth.routes.js";
+import appointmentRoutes from "./routes/appointment.routes.js";
+import healthRecordRoutes from "./routes/healthRecord.routes.js";
+import blogRoutes from "./routes/blog.routes.js";
+import queryRoutes from "./routes/query.routes.js";
+
+import { errorHandler, notFound } from "./middlewares/error.middleware.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "✅ Healthcare Wellness Portal API running..." });
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/health", healthRecordRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/queries", queryRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
